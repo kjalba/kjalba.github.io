50fd20c95903f88206bee4951efcc0af19aade1f
kjalba <albakevinjulian@gmail.com>
2025-02-09 20:22:08 -0600

    created ProjectCard component and got the Projects section layout set. Need to still update project names, descriptions, colors, header freezing, and test responsiveness

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,31 @@
+interface ProjectCardProps {
+  image: string;
+  title: string;
+  description: string;
+}
+
+export default function ProjectCard({ image, title, description }: ProjectCardProps) {
+  return (
+    <div className="w-full sm:w-full lg:w-1/2 px-4 mt-4">
+      <div className="project-card relative w-full h-[425px] bg-gray-200 cursor-pointer rounded-lg overflow-hidden group flex flex-col justify-center">
+        {/* Image Wrapper */}
+        <div className="relative w-full h-full flex items-center justify-center overflow-hidden">
+          <img
+            src={image}
+            alt={`${title} - Image`}
+            className="w-[70%] h-[75%] object-contain block"
+          />
+        </div>
+
+        {/* Translucent Overlay */}
+        <div className="absolute inset-0 bg-[#42776a] bg-opacity-50 opacity-0 transition-opacity duration-400 ease-in-out group-hover:opacity-50"></div>
+
+        {/* Overlay Text */}
+        <div className="absolute top-0 left-0 text-white pt-10 pl-10 max-w-[60%] leading-none opacity-0 transition-opacity duration-500 group-hover:opacity-50">
+          <h3 className="text-xl font-bold">{title}</h3>
+          <p className="text-sm">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,24 @@
+import ProjectCard from "./ProjectCard";
+import chessImg from "../assets/images/chess.png";
+import helixImg from "../assets/images/helix.png";
+
 export default function Projects() {
   return (
-    <>
-      <div className="container w-full px-4 mx-auto sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] mt-16 md:mt-24">
-        <h2 className="text-xl md:text-2xl font-bold mb-2 leading-tight">
-          My Projects.
-        </h2>
+    <section id="featured-work" className="container m:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] w-full mb-24 px-4 mx-auto max-w-screen-lg">
+      <h2 className="text-[22pt] font-bold leading-tight mb-6">My Projects.</h2>
+      <div className="flex flex-wrap -mx-4">
+        <ProjectCard
+          image={helixImg}
+          title="Helix Project"
+          description="A deep learning project with advanced modeling."
+        />
+        <ProjectCard
+          image={chessImg}
+          title="Chess Engine"
+          description="A chess AI built from scratch with FIDE rules."
+        />
+        {/* Add more <ProjectCard /> components as needed */}
       </div>
-    </>
-  )
+    </section>
+  );
 }
